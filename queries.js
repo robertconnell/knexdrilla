489ed123217872c0ab613197e2e3758b7e8c383a
Robert Connell <robertconnell@Kristinas-MacBook-Pro.local>
2018-08-08 11:08:41 -0600

    created knex drill a

---
+++ queries.js
@@ -0,0 +1,19 @@
+const database = require("./database-connection");
+
+module.exports = {
+    list(){
+        return database('resolution').select();
+    },
+    read(id){
+        return database('resolution').select().where('id', id).first();
+    },
+    create(resolution){
+        return database('resolution').insert(resolution).returning('*').then(record => record[0]);
+    },
+    update(id, resolution){
+        return database('resolution').update(resolution).returning('*').then(record => record[0]);
+    },
+    delete(id){
+        return database('resolution').delete().where('id', id);
+    }
+};
